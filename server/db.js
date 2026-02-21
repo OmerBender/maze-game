cba9a413ae2b8c3c85ea22023c4c9c68e351a105
Abraheem2010 <ibraheem.hassda@gmail.com>
2026-02-21 20:49:33 +0200

    Replace scores table with records table in db.js

--- server/db.js
+++ server/db.js
@@ -2,7 +2,11 @@ const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('./maze_records.db');
 
 db.serialize(() => {
-    db.run("CREATE TABLE IF NOT EXISTS scores (stage INTEGER PRIMARY KEY, name TEXT, time REAL)");
+    db.run(`CREATE TABLE IF NOT EXISTS records (
+        stage INTEGER PRIMARY KEY,
+        name TEXT NOT NULL,
+        time REAL NOT NULL
+    )`);
 });
 
 module.exports = db;
