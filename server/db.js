77e6bca5d81ef8108d9ba3ff955c727ea369c99d
OmerBender <Omerb127@gmail.com>
2026-02-22 12:07:24 +0200

    Refactor database path to absolute path
    Updated database path to use absolute path for maze_records.db.

--- server/db.js
+++ server/db.js
@@ -1,5 +1,12 @@
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./maze_records.db');
+const path = require('path');
+
+// יוצר נתיב מוחלט לקובץ בתוך תיקיית server
+const dbPath = path.join(__dirname, 'maze_records.db');
+
+console.log("Using DB at:", dbPath);
+
+const db = new sqlite3.Database(dbPath);
 
 db.serialize(() => {
     db.run(`CREATE TABLE IF NOT EXISTS records (
